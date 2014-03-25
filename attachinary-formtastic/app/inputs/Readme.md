d476d334af38f20a248c3005b0c21dcb84fac036
Filippos Vasilakis <vasilakisfil@gmail.com>
2014-03-25 11:58:48 +0100

    Add attachinary-formtastic input

---
+++ attachinary-formtastic/app/inputs/Readme.md
@@ -0,0 +1,1 @@
+It supports jquery borwser upload.
---
+++ attachinary-formtastic/app/inputs/attachinary_input.rb
@@ -0,0 +1,39 @@
+class AttachinaryInput
+  include Formtastic::Inputs::Base
+
+  def input_html_options
+    options = {}
+    options[:html] = {}
+    options[:html][:accept]= "image/jpeg,image/png,image/gif"
+    options[:html][:class] = "attachinary-input"
+    data_attachinary = @model_options.to_json
+    options[:html]["data-attachinary"] = data_attachinary
+
+    options[:cloudinary] ||= {}
+    options[:cloudinary][:tags] ||= []
+    options[:cloudinary][:tags]<< "#{Rails.env}_env"
+    options[:cloudinary][:tags]<< Attachinary::TMPTAG
+    options[:cloudinary][:tags].uniq!
+    cloudinary_upload_url = Cloudinary::Utils.cloudinary_api_url("upload",
+        {:resource_type=>:auto}.merge(options[:cloudinary]))
+    api_key = options[:cloudinary][:api_key] || Cloudinary.config.api_key || raise("Must supply api_key")
+    api_secret = options[:cloudinary][:api_secret] || Cloudinary.config.api_secret || raise("Must supply api_secret")
+
+    cloudinary_params = Cloudinary::Uploader.build_upload_params(options[:cloudinary])
+    cloudinary_params[:callback] = "http://localhost:3000/attachinary/cors"
+    cloudinary_params[:signature] = Cloudinary::Utils.api_sign_request(cloudinary_params, api_secret)
+    cloudinary_params[:api_key] = api_key
+    data_form_data = cloudinary_params.reject{ |k, v| v.blank? }.to_json
+    options[:html]["data-form-data"] = data_form_data
+    data_url = "https://api.cloudinary.com/v1_1/vasilakisfil/auto/upload"
+    options[:html]["data-url"] = data_url
+    super.merge(options[:html])
+
+  end
+
+  def to_html
+    @model_options = self.object.send("#{method}_metadata")
+    builder.file_field(method, input_html_options)
+  end
+end
+
